5e319319fffd16d0bb922c57f649150c224a7346
Aleksandr Tkachev <r91tap@gmail.com>
2024-01-25 19:42:44 +0300

    temperature

--- Humework3/EvenOdd/EvenOdd.cs
+++ Humework3/EvenOdd/EvenOdd.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Tracing;
+int i = 0;
+while (i == 0)
+{
+    try
+    {
+        Console.Write("Введите число чтоб определить его чётность:\t");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int result = number % 2;
+        if (result == 0)
+            Console.WriteLine("Число чётное.");
+        else
+            Console.WriteLine("Число нечётное.");
+        i++;
+
+    }
+    catch
+    {
+        Console.WriteLine("Неверные данные. \nВведите число:");
+    }
+    finally
+    {
+
+
+        Console.ReadKey();
+
+    }
+
+}
+
---
+++ Humework3/temperature/Program.cs
@@ -0,0 +1,26 @@
+﻿int i = 0;
+while (i == 0)
+{
+
+    try
+    {
+        Console.Write("Введите температуру на улице: \t");
+        int degree = Convert.ToInt32(Console.ReadLine());
+        if (degree > -5)
+            Console.WriteLine("Тепло");
+        else if (degree < -5 && degree > -20)
+            Console.WriteLine("Нормально");
+        else Console.WriteLine("Холодно");
+        i++;
+    }
+    catch
+    {
+        Console.Write("Неверные данные. \nВведите температуру по примеру \"-5,5,-10:\t");
+    }
+    finally
+    {
+        Console.ReadLine();
+    }
+}
+
+
