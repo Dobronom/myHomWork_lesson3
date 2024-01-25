80b782c7489efbdfb90927c44388008d19d9f4ea
Aleksandr Tkachev <r91tap@gmail.com>
2024-01-25 19:47:30 +0300

    calculator

---
+++ Humework3/calculator/Program.cs
@@ -0,0 +1,104 @@
+﻿//Visual studio - Console Application
+//Создать приложение калькулятор, выполняющая основные арифметические операции:
+
+//-Сложение
+//- Вычитание
+//- Деление
+//- Умножение
+//- Процент от числа
+//- Квадратный корень числа
+//- Отображение результата
+
+//По окончанию вычисления, диалог с пользователем с предложением о продолжении/окончании работы с программной
+//Сделать приложение максимально устойчивым
+
+
+
+
+using System;
+
+Console.WriteLine("Добро пожаловать. Ознакомтесь с органами управления калькулятора");
+Console.WriteLine("Сложение + , вычитание -, деление /, умножение *,\nпроценты от числа %, квадратный корень числа √(Для ввода символа зажать alt + 251 на NumPad)");
+
+int i = 0;
+while (i == 0)
+{
+    try
+    {
+
+        Console.WriteLine("Первое значение");
+        double value1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("укажите тип операции");
+        string action = Console.ReadLine();
+
+        if (action == "+")
+        {
+            Console.WriteLine("Второе значение");
+            double value2 = Convert.ToInt32(Console.ReadLine());
+            double result = value1 + value2;
+            Console.WriteLine("Ответ:\t" + result);
+            Console.WriteLine("если вы желаете закончить рабоу введите любое число кроме 0");
+            i = Convert.ToInt32(Console.ReadLine());
+
+        }
+        else if (action == "-")
+        {
+            Console.WriteLine("Второе значение");
+            double value2 = Convert.ToInt32(Console.ReadLine());
+            double result = value1 - value2;
+            Console.WriteLine("Ответ:\t" + result);
+            Console.WriteLine("если вы желаете закончить рабоу введите любое число кроме 0");
+            i = Convert.ToInt32(Console.ReadLine());
+        }
+        else if (action == "/")
+        {
+            Console.WriteLine("Второе значение");
+            double value2 = Convert.ToInt32(Console.ReadLine());
+            double result = value1 / value2;
+            Console.WriteLine("Ответ:\t" + result);
+            Console.WriteLine("если вы желаете закончить рабоу введите любое число кроме 0");
+            i = Convert.ToInt32(Console.ReadLine());
+        }
+        else if (action == "*")
+        {
+            Console.WriteLine("Второе значение");
+            double value2 = Convert.ToInt32(Console.ReadLine());
+            double result = value1 * value2;
+            Console.WriteLine("Ответ:\t" + result);
+            Console.WriteLine("если вы желаете закончить рабоу введите любое число кроме 0");
+            i = Convert.ToInt32(Console.ReadLine());
+        }
+        else if (action == "%")
+        {
+            Console.WriteLine("Второе значение");
+            double value2 = Convert.ToInt32(Console.ReadLine());
+            double result = (value2 / 100) * value1;
+            Console.WriteLine("Ответ:\t" + result);
+            Console.WriteLine("если вы желаете закончить рабоу введите любое число кроме 0");
+            i = Convert.ToInt32(Console.ReadLine());
+        }
+        else if (action == "√")
+        {
+            double result = Math.Sqrt(value1);
+            Console.WriteLine("Ответ:\t" + result);
+            Console.WriteLine("если вы желаете закончить рабоу введите любое число кроме 0");
+            i = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+
+    }
+    catch
+    {
+        Console.WriteLine("Сбой. Возврат к началу программы.\nНажмите Enter");
+    }
+    finally
+    {
+
+
+
+        Console.ReadLine();
+
+    }
+}
--- Humework3/rainbow/Program.cs
+++ Humework3/rainbow/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    Console.Write("Введите число от 1 до 7 чтоб узнать его цвет в радуге:\t");
+    int number = Convert.ToInt32(Console.ReadLine());
+    switch (number)
+    {
+        case 1:
+            Console.WriteLine("Красный");
+            break;
+        case 2:
+            Console.WriteLine("Оранжевый");
+            break;
+        case 3:
+            Console.WriteLine("Жёлтый");
+            break;
+        case 4:
+            Console.WriteLine("Зелёный");
+            break;
+        case 5:
+            Console.WriteLine("Голубой");
+            break;
+        case 6:
+            Console.WriteLine("Синий");
+            break;
+        case 7:
+            Console.WriteLine("Фиолетовый");
+            break;
+        default:
+            Console.WriteLine("Ведены неверные данные");
+            break;
+    }
+}
+catch
+{
+    Console.WriteLine("Ведены неверные данные");
+}
+finally
+{
+    Console.ReadLine();
+}
